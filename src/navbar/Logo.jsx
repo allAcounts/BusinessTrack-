87b4eaad0f4f24a6b45feb41b2172ed2c2a51f65
Yannick Wessidjam <yannickwessidjam@yannicks-mbp.myfiosgateway.com>
2023-02-07 16:45:40 -0500

    made a nav directory with a css file for that component and styles

---
+++ src/navbar/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logo = () => {
+
+  return(
+    <div className="topnav">
+      <h1>BusinessTrack</h1>
+      <a href="/" className="linklogin">Log In</a>
+    </div>
+  )
+}
+
+export default Logo; 
